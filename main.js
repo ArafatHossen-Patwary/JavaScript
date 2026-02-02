38391ba5668e4fae9093a3ed7fbba412ced2f993
Arafat Hossen Patwary <arafathn1299@gmail.com>
2026-02-03 02:01:20 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// console.log("Hello World");
+// console.error("this is an error");
+// console.warn('this is Warning');
+//..................................................
+// string, Number, Boolean, null, undefined
+
+// const name = 'Arafat';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof 'name');
+// console.log( typeof age );
+// console.log( typeof rating );
+// console.log( typeof isCool);
+// console.log( typeof x );
+// console.log( typeof y );
+// console.log( typeof z );
+//.....................................................
+// Concatination
+// const name = 'Arafat';
+// const age = 30;
+
+// console.log('My name is '+name+' i am '+age);
+//......................................................
+// Template String
+// const name = 'Arafat';
+// const age = 30;
+
+// console.log(`My name is ${name} and i am ${age}`);
+
+// const hello = `My name is ${name} and i am ${age}`;
+// console.log(hello);
+//........................................................
+// length of String
+// const s = 'Hello World';
+// console.log(s.length)
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+// console.log(s.substring(0, 5));
+// console.log(s.substring(0, 5). toUpperCase());
+//................................................
+// Split of string
+const s = "Apple, Banana, orange, guava";
+console.log(s.split(','))
+
+
+
