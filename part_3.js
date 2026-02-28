44b6dccf3d55f0f9560f048db180790931f0636b
Arafat Hossen Patwary <arafathn1299@gmail.com>
2026-03-01 02:55:19 +0600

    Add part_3.js

---
+++ part_3.js
@@ -0,0 +1,80 @@
+//Conditionals
+
+// const x = 5;
+// if(x === 10){
+//     console.log('x is 10');
+// }else if(x > 10){
+//     console.log('x is greter to 10'); 
+// }else{
+//     console.log('x is less to 10');
+// }
+//..........................................
+
+
+// let x = 4;
+// let y = 11;
+// if(x = 5 || y > 10){
+//     console.log('x is false but y is true');
+// }
+
+// let x = 4;
+// let y = 11;
+// if(x = 4 && y > 10){
+//     console.log('you are win');
+// }
+//...............................................
+// Turnary operator
+
+// const x = 12;
+
+// const color = x > 10 ? 'read' : 'blue';
+// console.log(color);
+//.................................................
+
+// const x = 12;
+
+// const color = x > 13 ? 'read' : 'blue';
+
+// switch(color){
+//     case'read':
+//        console.log('color is read');
+//        break;
+
+//     case'blue':
+//        console.log('color is blue');
+//        break;
+
+//     default:
+//         console.log('colore is not read or blue');
+//         break;
+// }
+//.....................................................
+
+//Functions
+
+// function addnums(num1, num2){
+//    console.log(num1 + num2);
+// }
+
+// addnums(5, 5);
+//.......................................
+
+
+// Arow functions
+// const addnums=(num1, num2)=>{
+//     return num1 + num2;
+// }
+
+// console.log(addnums(5,5));
+//.............................................
+// const addnums=(num1, num2)=>{
+//     console.log (num1 + num2);
+// }
+
+// addnums(5,6)
+//.............................................
+
+const addnums=(num1, num2)=> num1 + num2;
+
+console.log(addnums(5,6));
+
