93b3668c8af6a0337e08a6124889487e4f3f440b
Arafat Hossen Patwary <arafathn1299@gmail.com>
2026-03-01 02:52:33 +0600

    Add DOM.js

---
+++ DOM.js
@@ -0,0 +1,10 @@
+//select single element
+
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+console.log(document.querySelector('h1'));
+
+//select multiple element
+
+console.log(document.querySelectorAll('.items'));
+ 
